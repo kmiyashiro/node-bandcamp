12e5aa9901850d6091b743e041e6f0e50f44fa48
Kelly Miyashiro <miyashiro.kelly@gmail.com>
2011-07-15 18:06:57 -0700

    whitespace removal

--- test/test.js
+++ test/test.js
@@ -8,7 +8,7 @@ module.exports = {
     /**
      * Band module
      */
-     
+
     'Test .version': function() {
         bandcamp.version.should.match(/^\d+\.\d+\.\d+$/);
     },
@@ -61,61 +61,61 @@ module.exports = {
             result.should.have.keys('203035041', '3463798201');
         });
     },
-    
+
     /**
      * Album module
      */
-     
+
     'Album: Info works': function() {
         bandcamp.album.info('2587417518', function(err, result, status) {
             result = JSON.parse(result);
-    
+
             should.exist(result);
             status.should.equal(200);
             result.should.include.keys('tracks');
         });
     },
-    
+
     /**
      * Track module
      */
-     
+
     'Track: Info works': function() {
         bandcamp.track.info('1269403107', function(err, result, status) {
             result = JSON.parse(result);
-            
+
             should.exist(result);
             status.should.equal(200);
         });
     },
-    
+
     /**
      * URL module
      */
-     
+
     'URL: Info works': function() {
         // Band
         bandcamp.url.info('cults.bandcamp.com', function(err, result, status) {
             result = JSON.parse(result);
-            
+
             should.exist(result);
             status.should.equal(200);
             result.should.have.keys('band_id');
         });
-        
+
         // Album
         bandcamp.url.info('http://music.sufjan.com/album/all-delighted-people-ep', function(err, result, status) {
             result = JSON.parse(result);
-            
+
             should.exist(result);
             status.should.equal(200);
             result.should.have.keys('album_id', 'band_id');
         });
-        
+
         // Track
         bandcamp.url.info('http://laurashigihara.bandcamp.com/track/zombies-on-your-lawn', function(err, result, status) {
             result = JSON.parse(result);
-            
+
             should.exist(result);
             status.should.equal(200);
             result.should.have.keys('band_id', 'track_id');
